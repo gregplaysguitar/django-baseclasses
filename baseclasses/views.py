3eaccff7eb6bb8dad507c69e9a2d3749f4609538
Greg Brown <gregplaysguitar@gmail.com>
2011-06-10 13:38:13 +1200

    docstring

--- baseclasses/views.py
+++ baseclasses/views.py
@@ -2,6 +2,20 @@ from django.views.generic.base import TemplateView
 
 
 class ExtraContextView(TemplateView):
+    """Like the built in Templateview, but compensates for the missing 
+    extra_context argument which was available with the old-style generic views.
+    
+    Usage example:
+    
+        urlpatterns = patterns('',
+            (r'^myurl/$', ExtraContextView.as_view(
+                template_name='mytemplate.html',
+                extra_context={
+                    ...
+                },
+            )),
+    
+    """
     extra_context = {}
     
     def get_context_data(self, **kwargs):
