c8ed7d9f88bf8bd67ecf84ef624d6734db10237b
Greg Brown <gregplaysguitar@gmail.com>
2011-09-21 12:40:56 +1200

    added util from master

---
+++ baseclasses/util.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+
+def get_model_attr(instance, attr):
+    """Example usage: get_model_attr(instance, 'category__slug')"""
+    for field in attr.split('__'):
+        instance = getattr(instance, field)
+    return instance
+
+
+
+def next_or_prev_in_order(instance, prev=False, qs=None):
+    """Used to implement prev() and next() methods in the base models classes below."""
+    if not qs:
+        qs = instance.__class__.objects
+    if prev:
+        qs = qs.reverse()
+        lookup = 'lt'
+    else:
+        lookup = 'gt'
+    
+    q_list = []
+    prev_fields = []
+    if qs.model._meta.ordering:
+        ordering = list(qs.model._meta.ordering)
+    else:
+        ordering = []
+    
+    for field in (ordering + ['pk',]):
+        if field[0] == '-':
+            this_lookup = (lookup == 'gt' and 'lt' or 'gt')
+            field = field[1:]
+        else:
+            this_lookup = lookup
+        q_kwargs = dict([(f, get_model_attr(instance, f)) for f in prev_fields])
+        q_kwargs["%s__%s" % (field, this_lookup)] = get_model_attr(instance, field)
+        q_list.append(models.Q(**q_kwargs))
+        prev_fields.append(field)
+    try:
+        return qs.filter(reduce(models.Q.__or__, q_list))[0]
+    except IndexError:
+        return None
+
+
+
+
+
+class LambdaManager(models.Manager):
+    """LambdaManager is a simple manager extension that is instantiated with a callable.
+    
+    This callable is passed the query set by get_query_set so that it can perform any 
+    additional transformations to it such as eg filtering.
+    """
+    def __init__(self, f):
+        super(LambdaManager, self).__init__()
+        
+        self.transform = f
+        
+    def get_query_set(self):
+        return self.transform(super(LambdaManager, self).get_query_set())
+
