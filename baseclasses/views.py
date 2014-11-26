55f8828f5e80c692c474cc4b06478e15ef0e03d0
Greg Brown <gregplaysguitar@gmail.com>
2014-11-26 15:52:31 +1300

    can ExtraContextView now we're using jinja2

--- baseclasses/views.py
+++ baseclasses/views.py
@@ -1,33 +1,11 @@
-from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic.base import RedirectView
 from django.core.urlresolvers import reverse
-
-class ExtraContextView(TemplateView):
-    """Like the built in Templateview, but compensates for the missing 
-    extra_context argument which was available with the old-style generic views.
-    
-    Usage example:
-    
-        urlpatterns = patterns('',
-            (r'^myurl/$', ExtraContextView.as_view(
-                template_name='mytemplate.html',
-                extra_context={
-                    ...
-                },
-            )),
-    
-    """
-    extra_context = {}
-    
-    def get_context_data(self, **kwargs):
-        context = super(ExtraContextView, self).get_context_data(**kwargs)
-        context.update(self.extra_context)
-        return context
 
 
 def reverse_and_redirect(view, *args, **kwargs):
-    '''Returns a RedirectView which will redirect to a url determined by reversing the 
-       given parameters. The reverse is lazy in that it won't be evaluated until 
-       needed.'''
+    '''Returns a RedirectView which will redirect to a url determined by 
+       reversing the given parameters. The reverse is lazy in that it won't 
+       be evaluated until needed.'''
     
     class _Redirect(RedirectView):
         def get_redirect_url(self, **k):
