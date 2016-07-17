bbf2cc7ab161f1bee462fb3c8ead1cb63bae34ff
Greg Brown <greg@gregbrown.co.nz>
2016-07-18 11:44:07 +1200

    Update admin.py

--- baseclasses/admin.py
+++ baseclasses/admin.py
@@ -4,7 +4,8 @@ from .models import PREVIEW_FLAG
 class ContentModelAdminMixin(object):
     """Enables staff preview of non-live objects, in combination with
        ContentModelQuerySet.live(request). Note the view must pass a request to
-       this method or the preview won't work. """
+       this method or the preview won't work, and the mixin needs to come 
+       before admin.ModelAdmin in the parent classes. Requires Django 1.7. """
 
     def view_on_site(self, obj):
         url = obj.get_absolute_url()
